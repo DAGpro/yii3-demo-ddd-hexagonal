a3080c2b2d56eb44184f454d5d5e6213c735d0ae
Mahmud <magamedov2010@gmail.com>
2022-03-03 17:05:59 +0300

    Declare strict types

--- src/Blog/Application/Service/AppService/CommandService/ModerateCommentService.php
+++ src/Blog/Application/Service/AppService/CommandService/ModerateCommentService.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Blog\Application\Service\AppService\CommandService;
 
 use App\Blog\Application\Service\CommandService\ModerateCommentServiceInterface;
--- src/Blog/Application/Service/QueryService/ModerateCommentQueryServiceInterface.php
+++ src/Blog/Application/Service/QueryService/ModerateCommentQueryServiceInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Blog\Application\Service\QueryService;
 
 use App\Blog\Domain\Comment;
